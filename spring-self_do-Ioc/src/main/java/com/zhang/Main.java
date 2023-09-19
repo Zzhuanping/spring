3c56a7f1bc39c6a4b990202e3aa1a1158a2956bb
zhouhuanping <zhp1612968664@163.com>
2023-09-19 18:37:53 +0800

    2023年9月20日

--- spring-self_do-Ioc/src/main/java/com/zhang/Main.java
+++ spring-self_do-Ioc/src/main/java/com/zhang/Main.java
@@ -1,7 +1,16 @@
 package com.zhang;
 
+import com.zhang.jie.bean.AnnotationAppcationContext;
+import com.zhang.jie.bean.ApplicationContext;
+import com.zhang.jie.service.UserService;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ApplicationContext context =
+                new AnnotationAppcationContext("com.zhang.jie");
+        UserService userService = (UserService) context.getBean(UserService.class);
+                userService.add();
+
+
     }
 }
--- spring-self_do-Ioc/src/main/java/com/zhang/jie/service/UserServiceImp.java
+++ spring-self_do-Ioc/src/main/java/com/zhang/jie/service/UserServiceImp.java
@@ -1,10 +1,18 @@
 package com.zhang.jie.service;
 
+import com.zhang.jie.anno.Bean;
+import com.zhang.jie.anno.Di;
 import com.zhang.jie.dao.UserDao;
 
+@Bean
 public class UserServiceImp implements UserService {
+
+    @Di
+    private UserDao dao;
+
     @Override
     public void add() {
+        System.out.println("service do");
 
     }
 }
